d3ae662db20903c46c7f51f4fa6f825a1a2dc2f4
yves1986 <gnenessemin@gmail.com>
2026-07-15 21:09:35 +0200

    Fix: installer Chrome via apt-get

--- render-build.sh
+++ render-build.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Installation des dépendances
+# 1. Installer Chrome via apt-get
+apt-get update
+apt-get install -y wget gnupg
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+# 2. Installer les dépendances Node
 npm install
-
-# Définir le cache Puppeteer
-PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
-mkdir -p $PUPPETEER_CACHE_DIR
-
-# Installer Chrome via Puppeteer
-npx puppeteer browsers install chrome
-
-# Copier le cache si nécessaire
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
-    echo "...Copie du cache Puppeteer"
-    cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
-else
-    echo "...Stockage du cache Puppeteer"
-    cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
-fi
