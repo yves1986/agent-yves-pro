16edc21182bf6969865f1632c1e188917c4c59be
yves1986 <gnenessemin@gmail.com>
2026-07-15 21:50:27 +0200

    Remove Chrome installation from render-build.sh
    Removed Chrome installation steps from the script.

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 1. Installer Chrome via apt-get
-apt-get update
-apt-get install -y wget gnupg
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
-apt-get update
-apt-get install -y google-chrome-stable
-
-# 2. Installer les dépendances Node
+# Installation des dépendances Node
 npm install
